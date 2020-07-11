1787bbfe6fb4115d99d88dcc739b6d0332ead03a
Ashish <yesashishs@gmail.com>
2020-07-11 19:34:13 -0400

    VStack, HStack and ZStack

--- iOS-Sandbox/iOS-Sandbox/ContentView.swift
+++ iOS-Sandbox/iOS-Sandbox/ContentView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(
+            alignment: .center,
+            spacing: 100) {
+            Spacer()
+            Image("asset").resizable().frame(width: 108.0, height: 108.0)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Hi, Welcome to Sandbox")
+            }
+            Text("Elements are stacked on top of eachother, how exciting!!!!")
+            HStack{
+                Text("!This is a HStack...")
+                Text("...inside a VStack!")
+            }
+            ZStack{
+                Text("!!              !!")
+                Text("ZStack")
+            }
+            Text("Built with SwiftUI")
+            Spacer()
+        }
     }
 }
 
